f11b5926c16011693627292561266b9649790458
Benjamin <rivard.benjamin.28@gmail.com>
2022-11-25 22:41:07 +0100

    set up trpc endpoints & prisma model

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,8 +1,8 @@
 import { router } from "../trpc";
-import { exampleRouter } from "./example";
+import { shortUrlRouter } from "./shortUrl";
 
 export const appRouter = router({
-  example: exampleRouter,
+  shortUrl: shortUrlRouter,
 });
 
 // export type definition of API
