ac7c5b4677eec7c6a1d7f4d7a581b00692b81d86
Benjamin <rivard.benjamin.28@gmail.com>
2022-11-26 10:20:12 +0100

    handling host name using .env

--- src/components/UrlShortener.tsx
+++ src/components/UrlShortener.tsx
@@ -1,6 +1,4 @@
-import React, { useState } from "react"
-
-import Link from 'next/link';
+import React, { useState } from "react";
 
 import { trpc } from "../utils/trpc";
 
@@ -8,45 +6,67 @@ import { trpc } from "../utils/trpc";
  * A simple url shortener component that display a form and create a shorter version of the url
  */
 const UrlShortener: React.FC = () => {
+  const [id, setId] = useState("");
+  const [url, setUrl] = useState("");
 
-    const [id, setId] = useState('');
-    const [url, setUrl] = useState('');
+  const mutation = trpc.shortUrl.create.useMutation();
 
-    const mutation =
-        trpc.shortUrl.create.useMutation();
+  const shortUrl = trpc.shortUrl.getOne.useQuery(id);
 
-    const shortUrl = trpc.shortUrl.getOne.useQuery(id);
+  const submitHandler: React.DOMAttributes<HTMLFormElement>["onSubmit"] = (
+    event
+  ) => {
+    event.preventDefault();
 
-    const submitHandler: React.DOMAttributes<HTMLFormElement>['onSubmit'] = (event) => {
-        event.preventDefault();
+    mutation.mutate(url, {
+      onSuccess(data) {
+        if (data) setId(data.id);
+      },
+    });
+  };
 
-        mutation.mutate(url, {
-            onSuccess(data) {
-                if (data) setId(data.id);
-            },
-        });
-    }
+  return (
+    <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+      <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+        Shorten an <span className="text-[hsl(280,100%,70%)]">Url</span>
+      </h1>
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
+        <form
+          className="flex items-center justify-center gap-2"
+          method="post"
+          onSubmit={submitHandler}
+        >
+          <label className="text-white" htmlFor="url">
+            Url
+          </label>
+          <input
+            className="rounded-md border-2 border-zinc-800 bg-white px-4 py-2"
+            value={url}
+            onChange={(event) => setUrl(event.target.value)}
+            name="url"
+            id="url"
+            type="url"
+            required
+          />
+          <button
+            type="submit"
+            className="rounded-md border-2 border-zinc-800 bg-white p-2 focus:outline-none"
+          >
+            Zap ⚡
+          </button>
+        </form>
 
-    return <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            Shorten an  <span className="text-[hsl(280,100%,70%)]">Url</span>
-        </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-            <form className="flex gap-2 items-center justify-center" method='post' onSubmit={submitHandler}>
-                <label className="text-white" htmlFor="url" >Url</label>
-                <input className="px-4 py-2 rounded-md border-2 border-zinc-800 bg-white"
-                    value={url} onChange={event => setUrl(event.target.value)} name="url" id="url" type="url" required />
-                <button
-                    type="submit"
-                    className="p-2 bg-white rounded-md border-2 border-zinc-800 focus:outline-none"
-                >
-                    Zap ⚡
-                </button>
-            </form>
+        {shortUrl.data && (
+          <span className="bg-white">
+            Your short url :
+            <a href={shortUrl.data.targetUrl} rel="noreferrer" target="_blank">
+              http://${process.env.HOST}/{shortUrl.data.targetUrl}
+            </a>
+          </span>
+        )}
+      </div>
+    </div>
+  );
+};
 
-            {shortUrl.data && <span className="bg-white"> Your short url : <a href={shortUrl.data.targetUrl} target='_blank'>http://localhost:3000/{shortUrl.data.targetUrl}</a></span>}
-        </div>
-    </div >
-}
-
-export default UrlShortener;  
+export default UrlShortener;
